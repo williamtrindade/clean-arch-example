61bce27e09d9d481c5c5f66628252dd7b936c5a2
williamtrindade <williamtrindade777@gmail.com>
2022-05-04 21:46:20 -0300

    feat: Add Spring Web

---
+++ src/main/java/dev/williamtrindade/cleanarchexample/CleanArchExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.williamtrindade.cleanarchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/williamtrindade/cleanarchexample/CleanArchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.williamtrindade.cleanarchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
